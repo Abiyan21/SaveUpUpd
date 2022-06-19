38592b2e11dc0c19e4f7227a677f05a37eb53867
Abiyan Khan <abiyan.khan@student.ipso.ch>
2022-06-19 23:55:21 +0200

    Problem Kommentar

--- SaveUpUpd/SaveUpUpd/View/MainPage.xaml.cs
+++ SaveUpUpd/SaveUpUpd/View/MainPage.xaml.cs
@@ -15,6 +15,13 @@ namespace SaveUpUpd.View
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Einziges Grund wieso ich dies so programmiert habe ist wegen eines Problems auf Xamarins Seite. Das Property "Command" funktioniert seit 8 Jahren nicht richtig beim MenuItem.
+        /// Aus diesem Grund konnte ich nicht das MVVM Pattern richtig anwenden bei diesem Command.
+        /// Es ist einfach ein sehr simples Command das zur Language Seite weiterleitet, damit der Benutzer eine andere Sprache auswählen kann.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void MenuItem_OnClicked(object sender, EventArgs e)
         {
             await Application.Current.MainPage.Navigation.PushAsync(new LanguagePage());
